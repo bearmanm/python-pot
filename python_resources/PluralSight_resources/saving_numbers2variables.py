c5814f7ef7ed19f0111694f64ef8a411f2419609
Mike Bearman <mike.bearman@sky.uk>
2023-09-17 17:47:57 +0100

    created PluralSight_resources and script01.pysaving_numbers2variables.py and pushed to python_pot repo

--- python_resources/PluralSight_resources/saving_numbers2variables.py
+++ python_resources/PluralSight_resources/saving_numbers2variables.py
@@ -1,13 +1,21 @@
+#create a variable length, by assigning a value to it, when u create a variable a piece of memory on ur pc labelled 'length' stores the value '10' 
 length = 10
-#print(length)
+#print length to screen 
+print(length)
 #answer 10
 
+#create another variable width
 width = 255
-#print(width)
+print(width)
 #answer 10, 255
 
+#create a variable area and multiply the two varibales lenght times width
 area = length*width
 print(area)
 #answer 10, 255, 2550
 
-
+#these variables are stored in memory
+#arithmetic operators in python are + - * / 
+#the amount 10 is a whole number, python make the type of amount an integer 'int' 
+#if then set amount to a decimal value, python make the type of amount an 'float'
+ 
