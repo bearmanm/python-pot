2ed917967b8707f16e6c1d8797f8109b614b60e5
Mike Bearman <mike.bearman@sky.uk>
2023-09-17 17:46:12 +0100

    created PluralSight_resources and script01.py

---
+++ python_resources/PluralSight_resources/saving_numbers2variables.py
@@ -0,0 +1,13 @@
+length = 10
+#print(length)
+#answer 10
+
+width = 255
+#print(width)
+#answer 10, 255
+
+area = length*width
+print(area)
+#answer 10, 255, 2550
+
+
---
+++ python_resources/PluralSight_resources/script01.py
@@ -0,0 +1,2 @@
+total = 10 + 20 + 30
+print(total)
