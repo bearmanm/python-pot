ca5ab2598bc05e13923dad3895b0ac3d72a00d74
Mike Bearman <mike.bearman@sky.uk>
2023-09-19 20:55:47 +0100

    removed instructions leaving just syntax from greet_function.py

--- python_resources/sept23_month_1/greet_function.py
+++ python_resources/sept23_month_1/greet_function.py
@@ -1,28 +1,14 @@
-#You could use this code in the real world in a variety of ways, such as:
+#for logic, see sublime Python-greeting.py
 
-#To create a simple chatbot that greets users by name.
-#To write a script that automatically greets users when they log in to a system.
-#To create a personalized greeting for users of a website or app.
-#To add a personal touch to your code, such as by greeting users by name in your documentation or test code.
-
-#Here is an example of how you could use the code to create a simple chatbot that greets users by name:
 def greeting(name):
     print('Hello', name)
 
-# Start a loop to continuously greet users.
 while True:
-    # Get the user's name.
     user_name = input('Enter your name:\n')
-
-    # Greet the user by name.
     greeting(user_name)
-
-    # Ask the user if they want to continue.
     continue_greeting = input('Do you want to continue? (y/n):\n')
-
-    # If the user does not want to continue, break out of the loop.
+    
     if continue_greeting != 'y':
         break
 
-# Print a goodbye message.
 print('Goodbye!')
