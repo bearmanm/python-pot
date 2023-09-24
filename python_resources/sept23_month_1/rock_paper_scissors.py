29b0848c93100c0b67f4d7055d1a4c32d742e416
Mike Bearman <mike.bearman@sky.uk>
2023-09-24 15:00:26 +0100

    updated rock, paper, scissors.py script, version2 NOW WORKINGgit add .

--- python_resources/sept23_month_1/rock_paper_scissors.py
+++ python_resources/sept23_month_1/rock_paper_scissors.py
@@ -1,14 +1,15 @@
 computer_choice = 'scissors'
-user_choice = input('Do you wnat to play rock, paper or scissors Matt?\n')
+user_choice = input('Do you want to play rock, paper or scissors Matt?\n')
+user_choice = input('Great! do you want rock, paper,or scissors??\n')
 
-if computer_choice ==user_choice: 
+if computer_choice == user_choice: 
   print('No one Wins, we have tied')
 
-elif user_choice=='rock' and computer_choice =='scissors':
-    print('XXXX You are the WINNER! XXXX')
-elif user_choice=='paper' and computer_choice =='rock':
-    print('XXXX You are the WINNER! XXXX')
-elif user_choice=='scissors' and computer_choice =='paper':
-    print('XXXX You are the WINNER! XXXX')
+elif user_choice == 'rock' and computer_choice == 'scissors':
+ print("XXXX You are the WINNER! XXXX")
+elif user_choice == 'paper' and computer_choice == 'rock':
+ print("XXXX You are the WINNER! XXXX")
+elif user_choice == 'scissors' and computer_choice == 'paper':
+ print('XXXX You are the WINNER! XXXX')
 else:
     print('you are the losser, the computer wins this time!! :)')
