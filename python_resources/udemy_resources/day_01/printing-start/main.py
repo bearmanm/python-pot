5edb4cea77bf4a67dff6c5733666533c02ab38ca
Mike Bearman <mike.bearman@sky.uk>
2023-09-27 15:47:33 +0100

    Udemy day 1 Printing main.py created and pushed to printing-start dir

---
+++ python_resources/udemy_resources/day_01/printing-start/main.py
@@ -0,0 +1,11 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')") 
+
+#output
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print') 
+
+
+
