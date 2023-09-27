a844c77d916dca26c7153ba8add5ee540eba5d24
Mike Bearman <mike.bearman@sky.uk>
2023-09-27 20:09:26 +0100

    adding Udemy exercise3_input_function.py

---
+++ python_resources/udemy_resources/day_01/printing-start/Exercise3_Input_Function.py
@@ -0,0 +1,13 @@
+# input function nested inside the print statement
+print("Hello " + input("What is your name?\n"))
+
+#this is a program that will print the number of characters in a user's name. 
+print(len(input('What is your name?')))
+
+
+ 
+
+
+
+
+
