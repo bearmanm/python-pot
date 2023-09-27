b6088e72c7bb3423b092b5d23c773481512b8293
Mike Bearman <mike.bearman@sky.uk>
2023-09-27 13:41:22 +0100

    trying various statements 'while True' 'break' 'loop' in rock_paper_scissors.py as a test

--- python_resources/sept23_month_1/rock_paper_scissors.py
+++ python_resources/sept23_month_1/rock_paper_scissors.py
@@ -1,19 +1,44 @@
-computer_choice = 'scissors'
-user_choice = input('Do you want to play rock, paper or scissors Matt?\n')
-user_choice = input('Great! do you want rock, paper,or scissors??\n')
+#computer_choice = 'scissors'
+#user_choice = input('Do you want to play rock, paper or scissors Matt? (yes/no):\n')
+#if user_choice.lower() =="yes":
+  #print("Great! which do you want? Rock, Paper or Scissors?\n")
+#elif user_choice.lower() == "no":
+  #print("Chicken!!! you must be scared you are going to lose!")
+#else: 
+  #print("Invalid input. Please type yes or no")
+  
 
-if computer_choice == user_choice: 
-  print('No one Wins, we have tied')
 
-elif user_choice == 'rock' and computer_choice == 'scissors':
- print("XXXX You are the WINNER! XXXX")
-elif user_choice == 'paper' and computer_choice == 'rock':
- print("XXXX You are the WINNER! XXXX")
-elif user_choice == 'scissors' and computer_choice == 'paper':
- print('XXXX You are the WINNER! XXXX')
-else:
-    print('you are the losser, the computer wins this time!! :)')
+#computer_choice = 'scissors'
+#if computer_choice == user_choice: 
+  #print('No one Wins, we have tied')
+
+#elif user_choice == 'rock' and computer_choice == 'scissors':
+ #print("XXXX You are the WINNER! XXXX")
+#elif user_choice == 'paper' and computer_choice == 'rock':
+ #print("XXXX You are the WINNER! XXXX")
+#elif user_choice == 'scissors' and computer_choice == 'paper':
+ #print('XXXX You are the WINNER! XXXX')
+#else:
+    #print('you are the losser, the computer wins this time!! :)')
 
 # In summary, this code still simulates a Rock-Paper-Scissors game between the user and the computer. 
 # The user is now prompted twice for their choice, and the outcome of the game is determined based on the choices made by both parties.
 # still working on how to use the yes/no input 
+
+
+while True:
+ user_choice = input('Do you want to play rock, paper or scissors Matt? (yes/no):\n')
+
+ if user_choice.lower() == "yes":
+    user_choice = input("Great! Which do you want? Rock, Paper, or Scissors?\n")
+    if user_choice.lower() in ["rock", "paper", "scissors"]:
+        print("You chose {user_choice.capitalize()}!")
+        break
+    else: 
+     print("Invalid choice. Please choose Rock, Paper, or Scissors.")
+ elif user_choice.lower() == "no":
+    print("Chicken!!! You must be scared you are going to lose!")
+    break
+ else:
+    print("Invalid input. Please type yes or no")
