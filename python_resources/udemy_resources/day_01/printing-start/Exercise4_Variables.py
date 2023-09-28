42b6d641ba192a40b528d747a0573017991e9087
Mike Bearman <mike.bearman@sky.uk>
2023-09-28 21:22:50 +0100

    added code inside exercise4_variables.py

---
+++ python_resources/udemy_resources/day_01/printing-start/Exercise4_Variables.py
@@ -0,0 +1,9 @@
+# turning input into variable by adding name = in font of input
+
+name = input("What is your name?")
+print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
