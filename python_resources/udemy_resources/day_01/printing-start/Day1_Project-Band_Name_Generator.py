210798e93d67ccc239746dd0d3b65252142480ac
Mike Bearman <mike.bearman@sky.uk>
2023-09-29 18:32:27 +0100

    corrected order of sentence to Day1_Project-Band_Name_Generator.py

--- python_resources/udemy_resources/day_01/printing-start/Day1_Project-Band_Name_Generator.py
+++ python_resources/udemy_resources/day_01/printing-start/Day1_Project-Band_Name_Generator.py
@@ -1,4 +1,4 @@
-# use all that you have from day1 learnt to create a band name generator 
+# use all that you have learnt from day1 to create a band name generator 
 
 greeting = ("I'm going to create you a band name Amy using my new BN generator")
 print(greeting)
