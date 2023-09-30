74d353804e61293ecb066adfdd22b90ef2698534
Mike Bearman <mike.bearman@sky.uk>
2023-09-30 17:37:04 +0100

    created mathemathical operations.py

---
+++ python_resources/udemy_resources/day_02/mathematical_operations.py
@@ -0,0 +1,21 @@
+#PEMDAS - states order of proirity
+
+#Parentheses    () brackets
+#Exponents      **
+#Multiplication * /
+#Division
+#Addition        + -
+#Subtraction  
+
+
+#() brackets
+# **  Exponents
+# * /
+# + -      
+    
+
+3 + 5  #addition
+7 - 4  #subtraction
+3 * 3  #multiplication
+6 / 3  #division
+2 ** 3 #power of 
