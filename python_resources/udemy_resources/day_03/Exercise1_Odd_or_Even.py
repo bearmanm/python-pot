35e58090a0435d07cbe56b3ef310e2086c17c987
Mike Bearman <mike.bearman@sky.uk>
2023-10-05 18:55:19 +0100

    created day_03 dir: added day3_exercise1_odd_even, and if_else test senarios

---
+++ python_resources/udemy_resources/day_03/Exercise1_Odd_or_Even.py
@@ -0,0 +1,15 @@
+# if else odd or even statement
+
+number = int(input()) #21
+
+# If the number can be divided by 2 with 0 remainder.
+if number %2 ==0:
+  print("Odd")
+else:
+  print("Even")
+# = Odd
+  
+# The % operator is the modulo operator, which gives the remainder of the division. 
+# number %2 gives the remainder when number is divided by 2. 
+# If the remainder is not zero (i.e., number %2 is not equal to zero), it is evaluated as True. If the remainder is zero, it is evaluated as False.
+
