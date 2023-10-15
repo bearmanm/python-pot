49716d6c264a48ab611812a8cbe8e322e6f5e2f1
Mike Bearman <mike.bearman@sky.uk>
2023-10-15 16:40:05 +0100

    updating code in day03_test_page

--- python_resources/udemy_resources/day_03/test_page_delete_after.py
+++ python_resources/udemy_resources/day_03/test_page_delete_after.py
@@ -6,7 +6,7 @@ if hair_lenght >=12:
     dye = int(input("How many months ago since you last had it dyed?\n"))
     if dye >=6:
         print("You could do with your hair being dyed soon!")
-elif dye <=5: 
+    elif dye <=5: 
         print("ok, we'll have to wait a bit before we dye it again!")
 else: 
     print("your hair needs to GROW!")
