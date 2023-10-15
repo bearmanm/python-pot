2a1f1ad7ab19500bc9e53cc58e6591b944f1d228
Mike Bearman <mike.bearman@sky.uk>
2023-10-15 16:37:11 +0100

    added day03_Exercise3_leap_year program

---
+++ python_resources/udemy_resources/day_03/Exercise3_leap_year.py
@@ -0,0 +1,22 @@
+# Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February. 
+
+# This is how you work out whether if a particular year is a leap year.
+
+# on every year that is divisible by 4 with no remainder
+# except every year that is evenly divisible by 100 with no remainder
+# unless the year is also divisible by 400 with no remainder
+    
+year = int(input("enter the year\n")) # 1900 or 2000
+if year %4 ==0 and (year %100 !=0 or year %400 ==0): #two variables need to be the same for this to become a True or a False statement
+    print("Leap year")
+else:
+    print("Not leap year")
+# 1900/4=475 no reminder, so True
+# 1900/100=19 no remainder, so False (condition asks != NOT equal to)
+# 1900/400=4.75 with remainder, so False (condition asks ==0 no remainder)
+#True, False or False = else: print(Not leap year)
+
+# 2000/4=500 no reminder, so True
+# 2000/100=20 no remainder, so False (condition asks != NOT equal to)
+# 2000/400=5 with remainder, so True (condition asks ==0 no remainder)
+#True, False or True = if: print(leap year)
--- python_resources/udemy_resources/day_03/test_page_delete_after.py
+++ python_resources/udemy_resources/day_03/test_page_delete_after.py
@@ -1,5 +1,4 @@
-
-
+#test elif
 hair_lenght = int(input("what is you hair lenght in inches?\n"))
 
 if hair_lenght >=12: 
@@ -7,8 +6,52 @@ if hair_lenght >=12:
     dye = int(input("How many months ago since you last had it dyed?\n"))
     if dye >=6:
         print("You could do with your hair being dyed soon!")
-    elif dye <5: 
+elif dye <=5: 
         print("ok, we'll have to wait a bit before we dye it again!")
 else: 
     print("your hair needs to GROW!")
 
+
+#test leap_year
+#year = int(input("enter the year\n"))
+#if year %4 ==0:
+    #print("Leap year")
+#elif year %100 ==0:
+    #print("Not Leap")
+#elif year %400 ==0:
+    #print("Leap year!")
+#else:
+    #print("Not Leap year")
+    
+#test leap_year
+#year = int(input("enter the year\n"))
+#if year %4 ==0 and year %400 ==0:
+    #print("Leap year")
+#elif year %100 !=0:
+    # != NOT equal
+    #print("Not leap year")
+#else:
+    #print("Not leap year")
+    
+#test leap year
+#year = int(input())
+#if year % 4 ==0: 
+ #if year % 100 ==0:
+  #if year % 400 ==0:
+   #print("Leap year")
+  #else:
+   #print("Not Leap year")
+ #else:
+  #print("leap year")
+#else:
+ #print("NOt leap year")
+ 
+
+#test leap year
+year=int(input('What year is it?\n'))
+if year % 100 == 0 and year % 400 == 0:
+  print('It is a leap year. February has 29 days')
+elif year % 4 == 0 and year % 100 != 0:
+  print('It is a leap year. February has 29 days')
+else:
+  print('It is not a leap year. February has 28 days')
